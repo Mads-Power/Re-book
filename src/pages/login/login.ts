de8014174fc68001bac0068b24d32d0c9e835eba
Mads-Power <mr.kolkin@gmail.com>
2018-05-11 01:15:12 +0300

    color detailsand try cath login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { User } from '../../app/models/user';
 import{ AngularFireAuth } from "angularfire2/auth";
+import { HomePage } from '../home/home';
 
 
 
@@ -24,20 +25,52 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
   }
   async login(user:User){
+    
     //Henter bruker info fra firebae for å autentisere at brukeren er reistrert
     try{
     const result =  this.afAuth.auth
-    .signInWithEmailAndPassword(this.user.email, this.user.password);
-      console.log(result);
-     if(result){
-      this.navCtrl.push('HomePage');
-     }
-   }catch(e){
+    .signInWithEmailAndPassword(this.user.email, this.user.password)
+      .then(res => {
+        this.navCtrl.push('HomePage');
+      }, err => {
+        
+        switch (err['code']) { // SWITCH THE CODE RETURNED TO SEE WHAT MESSAGE YOU'LL DISPLAY
+          case "auth/wrong-password":
+           
+            let alert = this.alertCtrl.create({
+              title: 'Invalid Credentials!',
+              buttons: ['OK'] 
+            });
+            alert.present();
+            break;
+    
+          case "auth/user-not-found":
+           
+             alert = this.alertCtrl.create({
+              title: 'Invalid Credentials!',
+              buttons: ['OK']
+            });
+            alert.present();
+            break;
+    
+          case "auth/invalid-email":
+            
+             alert = this.alertCtrl.create({
+              title: 'Invalid Credentials!',
+              buttons: ['OK']
+          });
+            alert.present();
+            break;
+        }
+          
+        
+      });
+
+}catch(e){
         console.error(e);
         let alert = this.alertCtrl.create({
           title: 'Register or sign in!',
-          buttons: ['OK']
-         
+          buttons: ['OK']  
         });
         alert.present();
         
